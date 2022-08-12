32748250bb8dc9b50660a26da8f2933560cfb83f
Cyano Prem <cyano.prem@gmail.com>
2022-08-13 00:29:48 +0530

    Be able to add 2 numbers

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+const buttons = document.querySelectorAll("button");
+const display = document.querySelector('.display')
+
+
+buttons.forEach(click => click.addEventListener('click', CalcDisplay));
+
+let displayNumber = 0;
+let temp = 0;
+let total = 0;
+// let final = 0;
+let operator = "";
+
+function CalcDisplay (e) {
+    if (e.target.id === "+") {
+        operator = "+";
+        total = displayNumber;
+        displayNumber = 0;
+        temp = 0;
+    } else if (e.target.id === "="){
+        total = operate(total, displayNumber, operator);
+        display.textContent = total;
+    } 
+    else {
+    displayNumber = (temp * 10) + (parseInt(e.target.id));
+    display.textContent = displayNumber;
+    temp = displayNumber;
+    }
+}
+
+
+
+
 let add = (a,b) => a+b;
 let subtract = (a,b) => a-b;
 let multiply = (a,b) => a*b;
@@ -15,7 +47,7 @@ let operate = (a,b,operator) => {
     }
     return value;
 }
-
+/*
 let a = parseInt(prompt("Enter first number :"));
 
 let operator = prompt("Enter the operator :");
@@ -24,5 +56,5 @@ operator.toString();
 let b = parseInt(prompt("Enter the second number :"));
 
 alert(operate(a,b,operator));
-
+*/
 
